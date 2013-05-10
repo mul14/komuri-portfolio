dc983d555a23c6735524700d992c1a23d76b62b2
Mulia Arifandy Nasution <mul14.net@gmail.com>
2013-05-10 16:58:05 +0700

    Add composer, mustache, idiorm. Ganti template engine pakai mustache.

--- index.php
+++ index.php
@@ -1,105 +1,21 @@
-<!doctype html>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title>Komuri Projects</title>
+<?php
+require_once realpath(__DIR__ . '/vendor/autoload.php');
 
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css"
-          rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
-    <link href="assets/css/style.min.css" rel="stylesheet" />
-</head>
-<body>
+ORM::configure('mysql:host=localhost;dbname=komuri_portfolio');
+ORM::configure(array(
+    'username' => 'root',
+    'password' => '',
+));
 
-<header>
-    <div class="container">
-        <div class="title pull-left">
-            <span class="orange-text bold-text">&lsaquo;</span>/
-            <strong>KOMURI</strong> <span>PROJECTS</span>
-            <span class="orange-text bold-text">&rsaquo;</span>
-        </div>
+$portfolio = ORM::for_table('portfolio')->find_array();
 
-        <nav class="menu pull-right">
-            <div>
-                <a href="http://komuri.co.id">Komuri Home Page</a>
-            </div>
-        </nav>
-    </div>
-</header>
+$mustache = new Mustache_Engine(array(
+    'cache'           => dirname(__FILE__) . '/tmp/cache/mustache',
+    'cache_file_mode' => 0666,
+    'loader'          => new Mustache_Loader_FilesystemLoader(__DIR__ . '/views'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/views/partials'),
+));
 
-<section class="main" role="main">
-    <div class="container">
-        <div class="row">
-            <?php for ($i = 1; $i <= 12; $i++): ?>
-                <!-- BOX -->
-                <div class="span4 box">
-                    <div class="box-inner">
-                        <div class="image">
-                            <img src="http://placehold.it/480x320"/>
-                        </div>
-                        <div class="title">
-                            Title
-                        </div>
-                        <div class="description">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem consequatur eos fuga
-                            possimus
-                            reiciendis similique tempore veniam! Ad aliquam animi blanditiis hic, obcaecati perferendis
-                            quis
-                            rem ut? Blanditiis, nisi?
-                        </div>
-                        <div class="actions">
+$data['portfolio'] = $portfolio;
 
-                            <div class="btn-group">
-                                <a href="#" class="btn btn-small">
-                                    <i class="icon-eye-open"></i> Demo
-                                </a>
-                                <button class="btn btn-small dropdown-toggle" data-toggle="dropdown" data-target="#">
-                                    <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Home page</a></li>
-                                    <li><a href="#">Administrator</a></li>
-                                </ul>
-                            </div>
-
-                            <div class="btn-group">
-                                <a href="#" class="btn btn-small">
-                                    <i class="icon-book"></i> Manual
-                                </a>
-                                <button class="btn btn-small dropdown-toggle" data-toggle="dropdown" data-target="#">
-                                    <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Manual Book</a></li>
-                                </ul>
-                            </div>
-
-                            <a href="#" class="btn btn-small"><i class="icon-camera"></i> Screenshots</a>
-                        </div>
-                    </div>
-                </div>
-                <!--/ BOX -->
-            <?php endfor ?>
-        </div>
-    </div>
-</section>
-
-<footer>
-    <div class="container">
-        &copy; Komuri 2013
-    </div>
-</footer>
-
-<script src="//code.jquery.com/jquery-2.0.0.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-<script src="//masonry.desandro.com/jquery.masonry.min.js"></script>
-<script>
-    $(function () {
-        $('.main .container').masonry({
-            itemSelector: '.box',
-            isFitWidth: true
-        });
-    })
-</script>
-</body>
-</html>
+echo $mustache->render('template', $data);
