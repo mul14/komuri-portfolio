0f5878e0877be6437278c32c9f3a514031545f6c
Mulia Arifandy Nasution <mul14.net@gmail.com>
2013-05-10 19:22:39 +0700

    Add Phinx as migrations tool.

--- README.md
+++ README.md
@@ -4,8 +4,20 @@ Simple Portfolio template for [Komuri](http://komuri.co.id "Komuri website").
 
 [View Screenshot](https://bitbucket.org/mul14/komuri-portfolio/raw/048a383cb6278eabc86e0a918d47f6b08ad7090f/screenshot.png)
 
-## Install
+## Installation
+
+### Install Dependencies
 
 Run [composer](http://getcomposer.org "Composer website")
 
 `composer install`
+
+### Database Setup
+
+This application use [Phinx](http://phinx.org/) as migration tool.
+
+Edit file `phinx.yml`
+
+Run migration
+
+`./vendor/bin/phinx migrate`
---
+++ migrations/20130510182934_create_portfolio_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreatePortfolioTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $portfolio = $this->table('portfolio_');
+        $portfolio
+            ->addColumn('name', 'string', array('limit' => 255))
+            ->addColumn('url', 'string', array('limit' => 255))
+            ->addColumn('tags', 'binary')
+            ->addColumn('description', 'text')
+            ->addColumn('image_url', 'string', array('limit' => 255))
+            ->addColumn('project_date', 'datetime')
+            ->addColumn('is_active', 'boolean')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->save();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable('portfolio_');
+    }
+}
