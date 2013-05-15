4c240305b4a3938246e07babccbb92fabba9a8f2
Mulia Arifandy Nasution <mul14.net@gmail.com>
2013-05-15 18:56:52 +0700

    Little adjustment to public directory.

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,12 @@
 <?php
-require_once realpath(__DIR__ . '/vendor/autoload.php');
+define('ENVIRONMENT', 'development');
+define('ROOT_DIR', realpath(__DIR__));
+
+require_once ROOT_DIR.'/vendor/autoload.php';
 use \Symfony\Component\Yaml\Yaml as Yaml;
 
 $yaml     = new Yaml;
-$dbconfig = $yaml->parse(file_get_contents(__DIR__ . '/phinx.yml'));
+$dbconfig = $yaml->parse(file_get_contents(ROOT_DIR.'/phinx.yml'));
 $dbconfig = $dbconfig['environments'][ENVIRONMENT];
 
 $dsn = sprintf('%s:host=%s;port=%s;dbname=%s', $dbconfig['adapter'], $dbconfig['host'], $dbconfig['port'], $dbconfig['name']);
--- public/index.php
+++ public/index.php
@@ -1,15 +1,13 @@
 <?php
-define('ENVIRONMENT', 'development');
-
-require_once 'bootstrap.php';
+require_once '../bootstrap.php';
 
 $portfolio = ORM::for_table('portfolio')->find_array();
 
 $mustache = new Mustache_Engine(array(
-    'cache'           => dirname(__FILE__) . '/tmp/cache/mustache',
+    'cache'           => ROOT_DIR.'/tmp/cache/mustache',
     'cache_file_mode' => 0666,
-    'loader'          => new Mustache_Loader_FilesystemLoader(__DIR__ . '/views'),
-    'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/views/partials'),
+    'loader'          => new Mustache_Loader_FilesystemLoader(ROOT_DIR.'/views'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(ROOT_DIR.'/views/partials'),
 ));
 
 $data['portfolio'] = $portfolio;
